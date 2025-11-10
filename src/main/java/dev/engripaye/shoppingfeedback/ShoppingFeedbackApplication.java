9bcb13ea87ad27bfe43740a03a0b1abfbbc8fc1c
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-10 19:38:30 +0100

    init commit

---
+++ src/main/java/dev/engripaye/shoppingfeedback/ShoppingFeedbackApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.shoppingfeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingFeedbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/engripaye/shoppingfeedback/ShoppingFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.shoppingfeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingFeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
