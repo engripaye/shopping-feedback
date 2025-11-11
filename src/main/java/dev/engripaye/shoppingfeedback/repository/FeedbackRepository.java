40835c98fbc77efe925595cf7b4c58e56bc8a871
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-11 04:04:58 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/shoppingfeedback/repository/FeedbackRepository.java
+++ src/main/java/dev/engripaye/shoppingfeedback/repository/FeedbackRepository.java
@@ -1,4 +1,8 @@
 package dev.engripaye.shoppingfeedback.repository;
 
-public interface FeedbackRepository {
+import dev.engripaye.shoppingfeedback.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
 }
