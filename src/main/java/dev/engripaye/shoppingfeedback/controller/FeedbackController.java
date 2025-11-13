018b8309350386936936cd7148e9d1bb1acc01cf
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-13 06:42:59 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/shoppingfeedback/controller/FeedbackController.java
+++ src/main/java/dev/engripaye/shoppingfeedback/controller/FeedbackController.java
@@ -3,6 +3,8 @@ package dev.engripaye.shoppingfeedback.controller;
 import dev.engripaye.shoppingfeedback.dto.FeedbackRequest;
 import dev.engripaye.shoppingfeedback.model.Feedback;
 import dev.engripaye.shoppingfeedback.service.FeedbackService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/feedback")
+@Tag(name = "Feedback", description = "Endpoints for managing customer feedback")
 public class FeedbackController {
 
     private final FeedbackService service;
@@ -21,17 +24,22 @@ public class FeedbackController {
         this.service = service;
     }
 
+
+    @Operation(summary = "Submit feedback", description = "Saves a new feedback entry")
     @PostMapping
     public ResponseEntity<Feedback> submitFeedback(@Valid @RequestBody FeedbackRequest request) throws IOException, GeneralSecurityException {
         Feedback saved = service.createFeedback(request);
         return ResponseEntity.ok(saved);
     }
 
+    @Operation(summary = "Get all feedback", description = "Returns a list of all feedback entries")
     @GetMapping
     public ResponseEntity<List<Feedback>> allFeedback(){
         return ResponseEntity.ok(service.getAll());
     }
 
+
+    @Operation(summary = "Get feedback by ID", description = "Retrieves a specific feedback by its ID")
     @GetMapping("/{id}")
     public ResponseEntity<Feedback> get(@PathVariable Long id){
         Feedback f = service.getById(id);
